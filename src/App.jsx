390e3b66ecaab2474e8ea109b4cb624e6f3fe8a6
Eugene <eugenefung79@gmail.com>
2023-06-23 11:44:43 -0400

    stub up recruit menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <p>yo</p>
+      <div className='container'>
+        <div className='resources'>
+          <p>gems</p>
+          <p>tickets</p>
+        </div>
+        <div className='resources'>
+          <p>regular mile</p>
+          <p>special mile</p>
+        </div>
+        <div className='unitButtons'>
+          <button>probablity Info</button>
+          <button>revise wishlist</button>
+        </div>
+        <h1>Ordinary Recruit</h1>
+        <div>
+          <button>recruit1</button>
+          <button>recruit10</button>
+        </div>
+      </div>
     </>
   )
 }
