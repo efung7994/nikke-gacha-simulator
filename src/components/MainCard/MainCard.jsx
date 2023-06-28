0326f2db576eed1f85192419a4b5728e143677c8
Eugene <eugenefung79@gmail.com>
2023-06-28 12:23:32 -0400

    add Nikke dataset and change main page component

---
+++ src/components/MainCard/MainCard.jsx
@@ -0,0 +1,29 @@
+import './MainCard.css'
+
+const MainPage = () => {
+  return (
+  <>
+    <div className='container'>
+      <div className='resources'>
+        <p>gems</p>
+        <p>tickets</p>
+      </div>
+      <div className='resources'>
+        <p>regular mile</p>
+        <p>special mile</p>
+      </div>
+      <div className='unitButtons'>
+        <button>probablity Info</button>
+        <button>revise wishlist</button>
+      </div>
+      <h1>Ordinary Recruit</h1>
+      <div>
+        <button>recruit1</button>
+        <button>recruit10</button>
+      </div>
+    </div>
+  </>
+  )
+}
+
+export default MainPage
---
+++ src/data/data.js
@@ -0,0 +1,361 @@
+export const poolData = [
+  {
+    name: 'Admi',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Alice',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Anis',
+    rarity: 'SR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Aria',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Belorta',
+    rarity: 'SR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Biscuit',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Brid',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Centi',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Cocoa',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Crow',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'D',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Delta',
+    rarity: 'SR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Diesel',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Dolla',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Drake',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Emma',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Epinel',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Ether',
+    rarity: 'SR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Eunhwa',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Exia',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Folkwang',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Frima',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Guillotine',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Harran',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Helm',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Idoll Flower',
+    rarity: 'R',
+    img: 'admi.png',
+  },
+  {
+    name: 'Idoll Ocean',
+    rarity: 'r',
+    img: 'admi.png',
+  },
+  {
+    name: 'Idoll Sun',
+    rarity: 'R',
+    img: 'admi.png',
+  },
+  {
+    name: 'Isabel',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Jackal',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Julia',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Laplace',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Liter',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Ludmilla',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Maiden',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Mary',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Maxwell',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Mica',
+    rarity: 'SR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Mihara',
+    rarity: 'SR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Milk',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },{
+    name: 'Miranda',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Modernia',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'N102',
+    rarity: 'SR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Neon',
+    rarity: 'SR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Noah',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Noise',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Novel',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Pepper',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Poli',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Privaty',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Product 08',
+    rarity: 'R',
+    img: 'admi.png',
+  },
+  {
+    name: 'Product 12',
+    rarity: 'R',
+    img: 'admi.png',
+  },
+  {
+    name: 'Product 23',
+    rarity: 'R',
+    img: 'admi.png',
+  },
+  {
+    name: 'Rapi',
+    rarity: 'SR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Rapunzel',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Rupee',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Sakura',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Scarlet',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Signal',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Snow White',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Soda',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Soldier EG',
+    rarity: 'R',
+    img: 'admi.png',
+  },
+  {
+    name: 'Soldier FA',
+    rarity: 'R',
+    img: 'admi.png',
+  },
+  {
+    name: 'Soldier OW',
+    rarity: 'R',
+    img: 'admi.png',
+  },
+  {
+    name: 'Soline',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Sugar',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Vesti',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Viper',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Volume',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Yan',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Yulha',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+  {
+    name: 'Yuni',
+    rarity: 'SSR',
+    img: 'admi.png',
+  },
+ ]
